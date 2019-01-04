e1fcecfc62ed2acd582fcc02c3107f027ff5b1e3
Hugh Francis <hugh@sanctuary.computer>
2019-01-04 13:51:16 -0500

    Trim unused

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default () => (
   <section className="hero is-dark">
     <div className="hero-body">
       <div className="container">
-        <h1 className="title">Instagram for Smells</h1>
+        <h1 className="title">sanctucompu</h1>
         <Link to="/" className='button'>Home</Link>
         <Link to="/auth" className='button'>Login</Link>
         <Link to="/profile" className='button'>Account</Link>
